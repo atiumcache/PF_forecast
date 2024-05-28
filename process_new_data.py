b9e5525a78baba07448f5154b3bad6c29cecf3c9
atiumcache <aattilio@pm.me>
2024-05-28 15:28:57 -0700

    process new hosp data files. split by state.

--- process_new_data.py
+++ process_new_data.py
@@ -1,14 +1,15 @@
 import pandas as pd
-
+import os
 
 def main():
     location_to_state = {}
 
     full_data_file = './COVID-19_Reported_Patient_Impact_and_Hospital_Capacity_by_State_Timeseries__RAW_.csv'
     destination_folder = './datasets/hosp_data/'
+    os.makedirs(destination_folder, exist_ok=True)
 
     '''Import locations'''
-    locations = pd.read_csv('./locations.csv',skiprows=0) 
+    locations = pd.read_csv('./datasets/locations.csv',skiprows=0) 
     locations = locations.drop([0]) #skip first row (national ID)
     print("Number of Locations:", len(locations))
 
@@ -21,9 +22,14 @@ def main():
     '''Extract hospitalization data'''
     global full_hosp_data
     full_hosp_data = pd.read_csv(full_data_file) 
-    full_hosp_data = full_hosp_data[['date','state','previous_day_admission_influenza_confirmed']].sort_values(['state','date'])
+    full_hosp_data = full_hosp_data[['date','state','previous_day_admission_influenza_confirmed']]
     full_hosp_data['date'] = pd.to_datetime(full_hosp_data['date'])
 
+    '''Output each state's data to a separate csv file'''
+    for state_code, abbreviation in location_to_state.items():
+        state_data = full_hosp_data[full_hosp_data['state'] == abbreviation].sort_values(by=['date'])
+        output_filepath = destination_folder + 'hospitalizations_' + abbreviation + '.csv'
+        state_data.to_csv(output_filepath)
 
 if __name__ == '__main__':
     main()
